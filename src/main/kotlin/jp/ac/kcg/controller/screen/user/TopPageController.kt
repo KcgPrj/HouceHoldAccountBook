1786c52dfc0c70f10e1366401d9371e0c3d95340
orekyuu <orekyuu@yahoo.co.jp>
2016-05-28 00:34:41 +0900

    テストが落ちていたので修正

--- src/main/kotlin/jp/ac/kcg/controller/screen/user/TopPageController.kt
+++ src/main/kotlin/jp/ac/kcg/controller/screen/user/TopPageController.kt
@@ -4,16 +4,18 @@ import org.springframework.security.oauth2.provider.OAuth2Authentication
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import java.security.Principal
 
 @Controller
 class TopPageController {
 
     @GetMapping("/")
-    fun showTopPage(principal: OAuth2Authentication?, model: Model): String {
+    fun showTopPage(principal: Principal?, model: Model): String {
         model.addAttribute("principal", principal)
-        principal?.let {
+        (principal as? OAuth2Authentication)?.let {
             //TODO: 実際使う時のデモとしてコメントアウトしてある。 不要になったら消してね
             //service.findUserOrCreate(it.oAuth2Request.clientId, it.userAuthentication.name)
+            println("hogehoge: " + it.oAuth2Request.clientId)
         }
         return "index"
     }
